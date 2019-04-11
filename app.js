a84e7bad62fe20060035c3592b6a7b836abb3a44
github username <github email address>
2019-04-11 18:50:56 -0400

    added event listener for button

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
+//   BUDGET CONTROLLER
 var budgetController = (function(){
 
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+// some code
 
 })();
 
 
+//  UI CONTROLLER
 var UIController = (function(){
 
     //Some code
@@ -22,14 +14,22 @@ var UIController = (function(){
 })();
 
 
+
+//   GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-  var z = budgetCtrl.publicTest(5);
+    document.querySelector('.add__btn').addEventListener('click', function(){
 
-  return {
-      anotherPublic: function(){
-          console.log(z);
-      }
-  }
+      // 1. Get the field input data
+
+      // 2. Add the item to the budget controller
+
+      // 3. Add the item to the UI
+
+      // 4. Calculate the budget
+
+      // 5. Display the budget on the UI
+
+    });
 
 })(budgetController, UIController);
